e63b6a2f4d4e5e7652b34654eed6e5ff7c9b65b2
chloe alcorn <ca.alcorn@outlook.com>
2018-10-30 16:06:22 +0000

    read me file edited

---
+++ src/components/SongList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SongList extends React.Component{
+
+render(){
+
+  }
+}
+
+
+export default SongList;
--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -4,15 +4,23 @@ import React from 'react';
 class SongContainer extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      entry:[]
+    }
 
   };
+componentDidMount(){
+  fetch('https://itunes.apple.com/gb/rss/topsongs/limit=20/json')
+  .then(response => response.json())
+  .then(json => this.setState({entry: json}))
 
+}
 
 render(){
   return(
   <div>
     <h2>Top 20 Songs</h2>
-
+    <SongList entry={this.state.entry} />
   </div>
     );
   }
